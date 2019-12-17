bf616488be0dcab9d8d3f020a1cf62451b71cb63
Nory Lazo <nlazo@icloud.com>
2019-12-17 06:44:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,10 @@ require_relative './course.rb'
 
 class Scraper
   
+   def get_page 
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,18 +20,6 @@ class Scraper
     end
   end
   
-  def get_page 
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-    # doc.css(".post").each do |post|
-    #   course = Course.new 
-    #   course.title = post.css("h2").text
-    #   course.schedule = post.css(".date").text 
-    #   course.description = post.css("p").text
-    # end 
-    
-    # binding.pry
-  end 
   
   def get_courses 
     self.get_page.css(".post")
